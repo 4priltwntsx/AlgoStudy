244a3e72b280ebe9caa9f61526c950e35a482806
김지희 <cjg05034@naver.com>
2023-05-29 03:22:37 +0900

    solving: boj 1260 dfs와bfs

--- study2023/src/boj/BOJ_1260_DFS와BFS.java
+++ study2023/src/boj/BOJ_1260_DFS와BFS.java
@@ -12,6 +12,7 @@ public class BOJ_1260_DFS와BFS {
 	static int[][] map;
 	static int[] visit;
 	static StringBuilder sb;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -19,7 +20,7 @@ public class BOJ_1260_DFS와BFS {
 		M = Integer.parseInt(st.nextToken());
 		V = Integer.parseInt(st.nextToken());
 
-		map = new int[N+1][N+1];
+		map = new int[N + 1][N + 1];
 		for (int m = 0; m < M; m++) {
 			st = new StringTokenizer(br.readLine());
 			int i = Integer.parseInt(st.nextToken());
@@ -27,39 +28,39 @@ public class BOJ_1260_DFS와BFS {
 			map[i][j] = 1;
 			map[j][i] = 1;
 		}
-		
+
 		sb = new StringBuilder();
-		visit = new int[N+1];
+		visit = new int[N + 1];
 		dfs(V);
 		sb.append("\n");
-		visit = new int[N+1];
+		visit = new int[N + 1];
 		bfs(V);
 		System.out.print(sb.toString());
 	}
-	 
+
 	private static void dfs(int vertex) {
 		visit[vertex] = 1;
 		sb.append(vertex).append(" ");
-		if(vertex==N+1) {
+		if (vertex == N + 1) {
 			return;
 		}
-		for(int i=1; i<N+1; i++) {
-			if(map[vertex][i]==1 && visit[i]==0) {
+		for (int i = 1; i < N + 1; i++) {
+			if (map[vertex][i] == 1 && visit[i] == 0) {
 				visit[i] = 1;
 				dfs(i);
 			}
 		}
 	}
-	
+
 	private static void bfs(int vertex) {
 		Queue<Integer> Q = new LinkedList<Integer>();
 		Q.add(vertex);
 		visit[vertex] = 1;
 		sb.append(vertex).append(" ");
-		while(!Q.isEmpty()) {
+		while (!Q.isEmpty()) {
 			int cur = Q.poll();
-			for(int i=1; i<N+1; i++) {
-				if(map[cur][i]==1 && visit[i]==0) {
+			for (int i = 1; i < N + 1; i++) {
+				if (map[cur][i] == 1 && visit[i] == 0) {
 					visit[i] = 1;
 					Q.add(i);
 					sb.append(i).append(" ");
