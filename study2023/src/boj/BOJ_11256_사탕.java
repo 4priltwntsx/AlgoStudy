8c6134bb15a20cc16309636cf82a8b0fc5466942
김지희 <cjg05034@naver.com>
2023-05-30 03:22:15 +0900

    solving: boj 11256 사탕

--- study2023/src/boj/BOJ_11256_사탕.java
+++ study2023/src/boj/BOJ_11256_사탕.java
@@ -7,17 +7,17 @@ import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class BOJ_11256_사탕 {
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
+
 		int TC = Integer.parseInt(br.readLine());
-		for(int tc=0; tc<TC; tc++) {
+		for (int tc = 0; tc < TC; tc++) {
 			st = new StringTokenizer(br.readLine());
 			int J = Integer.parseInt(st.nextToken());
 			int N = Integer.parseInt(st.nextToken());
 			Long[] boxes = new Long[N];
-			for(int i=0; i<N; i++) {
+			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
 				long R = Long.parseLong(st.nextToken());
 				long C = Long.parseLong(st.nextToken());
@@ -25,25 +25,24 @@ public class BOJ_11256_사탕 {
 			}
 			Arrays.sort(boxes, new Comparator<Long>() {
 				public int compare(Long l1, Long l2) {
-					return (int) (l2-l1);
+					return (int) (l2 - l1);
 				}
 			});
 			int answer = 0;
-			for(Long size : boxes) {
-				if(J==0) break;
-				if(size>=J) {
+			for (Long size : boxes) {
+				if (J == 0)
+					break;
+				if (size >= J) {
 					answer++;
 					break;
-				}else if(size<J) {
-					J-=size;
+				} else if (size < J) {
+					J -= size;
 					answer++;
 				}
 			}
-			
+
 			System.out.println(answer);
 		}
-		
-		
-		
+
 	}
 }
