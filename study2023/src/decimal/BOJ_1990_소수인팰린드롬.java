d58055e71f0e6b9e65d4062a752085548d92d372
김지희 <cjg05034@naver.com>
2023-06-04 03:20:03 +0900

    solving: boj 1990 소수인팰린드롬

--- study2023/src/decimal/BOJ_1990_소수인팰린드롬.java
+++ study2023/src/decimal/BOJ_1990_소수인팰린드롬.java
@@ -7,23 +7,24 @@ import java.util.StringTokenizer;
 public class BOJ_1990_소수인팰린드롬 {
 	static int A, B;
 	static boolean[] prime;
-	public static void main(String[] args) throws Exception{
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		A = Integer.parseInt(st.nextToken());
 		B = Integer.parseInt(st.nextToken());
 		StringBuilder answer = new StringBuilder();
-		prime = new boolean[B+1];
+		prime = new boolean[B + 1];
 		prime[0] = prime[1] = true;
 		sieve();
-		
-		for(int i=A; i<=B; i++) {
-			if(!prime[i]) {
+
+		for (int i = A; i <= B; i++) {
+			if (!prime[i]) {
 				StringBuilder sb = new StringBuilder();
 				sb.append(i);
 				String input = sb.toString();
 				String output = sb.reverse().toString();
-				if(output.equals(input)) {
+				if (output.equals(input)) {
 					answer.append(i).append("\n");
 				}
 			}
@@ -32,18 +33,21 @@ public class BOJ_1990_소수인팰린드롬 {
 		answer.append(-1);
 		System.out.print(answer.toString());
 	}
+
 	public static boolean isPrime(int num) {
-		if(num==1) return false;
-		for(int i=2; i<=Math.sqrt(num); i++) {
-			if(num%i==0) return false;
+		if (num == 1)
+			return false;
+		for (int i = 2; i <= Math.sqrt(num); i++) {
+			if (num % i == 0)
+				return false;
 		}
 		return true;
 	}
-	
+
 	public static void sieve() {
-		for(int i=2; i*i<=B;i++) {
-			if(!prime[i]) {
-				for(int j=i*i;j<=B; j+=i) {
+		for (int i = 2; i * i <= B; i++) {
+			if (!prime[i]) {
+				for (int j = i * i; j <= B; j += i) {
 					prime[j] = true;
 				}
 			}
