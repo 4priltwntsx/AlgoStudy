b29c9b3d9206f6a9122ea3e4f92c9a94afe2b438
김지희 <cjg05034@naver.com>
2023-06-05 03:19:39 +0900

    solving: boj 1978 소수찾기

--- study2023/src/decimal/BOJ_1978_소수찾기.java
+++ study2023/src/decimal/BOJ_1978_소수찾기.java
@@ -5,23 +5,29 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ_1978_소수찾기 {
-	public static void main(String[] args) throws Exception{
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int answer = 0;
-		for(int n=0; n<N; n++) {
+		for (int n = 0; n < N; n++) {
 			int num = Integer.parseInt(st.nextToken());
-			if(isPrime(num)) answer++;
+			if (isPrime(num))
+				answer++;
 		}
 		System.out.print(answer);
 	}
+
 	public static boolean isPrime(int num) {
-		if(num==1) return false;
-		if(num==2) return true;
-		
-		for(int i=2; i<=Math.sqrt(num);i++) {
-			if(num%i==0) return false;
+		if (num == 1)
+			return false;
+		if (num == 2)
+			return true;
+
+		for (int i = 2; i <= Math.sqrt(num); i++) {
+			if (num % i == 0)
+				return false;
 		}
 		return true;
 	}
