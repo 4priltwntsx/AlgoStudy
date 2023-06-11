fbd7e70d5f2cea7595569bd834ccd33196b1f34b
김지희 <cjg05034@naver.com>
2023-06-12 03:26:47 +0900

    solving: boj 16953 A to B

--- study2023/src/boj/BOJ_16953_AtoB.java
+++ study2023/src/boj/BOJ_16953_AtoB.java
@@ -10,29 +10,32 @@ public class BOJ_16953_AtoB {
 	static long A, B;
 	static boolean visit[];
 	static long answer;
-	public static void main(String[] args) throws Exception{
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		A = Long.parseLong(st.nextToken());
 		B = Long.parseLong(st.nextToken()); // target
 		answer = 0;
 		bfs();
-		System.out.println(answer+1);
+		System.out.println(answer + 1);
 	}
-	
+
 	public static void bfs() {
 		Queue<Long> Q = new LinkedList<>();
 		Q.add(A);
-		
-		while(!Q.isEmpty()) {
+
+		while (!Q.isEmpty()) {
 			int size = Q.size();
-			
-			for(int s= 0; s<size; s++) {
+
+			for (int s = 0; s < size; s++) {
 				long cur = Q.poll();
-				if(cur>B) continue;
-				if(cur==B) return;
-				Q.add(cur*2);
-				Q.add(cur*10+1);
+				if (cur > B)
+					continue;
+				if (cur == B)
+					return;
+				Q.add(cur * 2);
+				Q.add(cur * 10 + 1);
 			}
 			answer++;
 		}
