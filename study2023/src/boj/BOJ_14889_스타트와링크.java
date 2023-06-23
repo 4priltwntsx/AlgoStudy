799b8f38170091f4f8e518845bd7a148f0a0284d
김지희 <cjg05034@naver.com>
2023-06-24 00:13:49 +0900

    add files

---
+++ study2023/src/boj/BOJ_14889_스타트와링크.java
@@ -0,0 +1,57 @@
+package boj;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ_14889_스타트와링크 {
+	static int N, capability[][];
+	static int[] team;
+	static int answer;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		N = Integer.parseInt(br.readLine());
+		capability = new int[N + 1][N + 1];
+		team = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= N; j++) {
+				capability[i][j] = Integer.parseInt(st.nextToken());
+			}
+		} // input end
+
+		answer = Integer.MAX_VALUE;
+		dfs(0, 1);
+		System.out.println(answer);
+	}
+
+	public static void dfs(int cnt, int idx) {
+		if (cnt == N / 2) {
+			int sumStart = 0;
+			int sumLink = 0;
+			for (int j = 1; j < N; j++) {
+				for(int k=j+1; k<=N; k++) {
+					if(team[j]==1  && team[k]==1) {
+						sumStart +=capability[j][k];
+						sumStart +=capability[k][j];
+					}
+					if(team[j]==0 && team[k]==0) {
+						sumLink += capability[j][k];
+						sumLink += capability[k][j];
+					}
+				}
+			}
+			int sub = Math.abs(sumStart-sumLink);
+			if(sub<=answer) answer = sub;
+			return;
+		}
+		if(answer==0) System.out.println(0);
+
+		for (int i = idx; i <= N; i++) { // team Start의 조합 구하기
+			team[i] = 1;
+			dfs(cnt + 1, idx + 1);
+			team[i] = 0;
+		}
+	}
+}
---
+++ study2023/src/boj/BOJ_2839_설탕배달.java
@@ -0,0 +1,27 @@
+package boj;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ_2839_설탕배달 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int result = 0;
+		while(true) {
+			if(N%5==0) {
+				result += N/5;
+				System.out.println(result);
+				break;
+			}
+			else {
+				N-=3;
+				result++;
+			}
+			if(N<0) {
+				System.out.println(-1);
+				break;
+			}
+		}
+	}
+}
