1eca5e62977eb3d6b7b19872c91ef49fa63f2d57
김지희 <cjg05034@naver.com>
2023-07-18 10:10:00 +0900

    solving: boj 16767 배월복원하기

--- study2023/src/Implement/BOJ_16767_배열복원하기.java
+++ study2023/src/Implement/BOJ_16767_배열복원하기.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ_16767_배열복원하기 {
+	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
