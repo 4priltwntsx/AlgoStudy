f80a8d12a72ab278a4bda949d668cd51b20bafb4
김지희 <cjg05034@naver.com>
2023-09-26 21:15:08 +0900

    solving: 프로그래머스 구명보트

---
+++ study2023/src/greedy/Solution_구명보트.java
@@ -0,0 +1,18 @@
+package greedy;
+
+public class Solution_구명보트 {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int index = 0;
+        for(int i=people.length-1; i>=index; i--){
+            if(people[i] + people[index] <= limit){
+                index++;
+                answer++;
+            }else{
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
