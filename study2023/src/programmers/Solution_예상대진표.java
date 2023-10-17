f0cf30b91a24391582ab5e535fe3b062909779a1
김지희 <cjg05034@naver.com>
2023-10-17 16:24:07 +0900

    solving: programmers 예상대진표

---
+++ study2023/src/programmers/Solution_예상대진표.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class Solution_예상대진표 {
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+
+        while(true){
+            answer++;
+            a = (a+1)/2;
+            b = (b+1)/2;
+            if(a==b) break;
+        }
+
+        return answer;
+    }
+}
