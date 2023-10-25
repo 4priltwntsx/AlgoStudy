a5c53e2e0eeb98ed23dfe376c38694784f32bdb2
Jihee Kim <51085309+4priltwntsx@users.noreply.github.com>
2023-10-25 17:38:32 +0900

    [Bronze V] Title: A+B, Time: 108 ms, Memory: 12876 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sd = new Scanner(System.in);
+        
+        int a = 0;
+        int b = 0;
+        
+        a = sd.nextInt();
+        b = sd.nextInt();
+        
+        System.out.println(a+b);
+    }
+}
