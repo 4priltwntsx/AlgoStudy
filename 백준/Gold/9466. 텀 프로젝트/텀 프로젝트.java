32949357f66404e0cdae4193ae09a7b33e3c1319
Jihee Kim <51085309+4priltwntsx@users.noreply.github.com>
2023-10-30 00:36:16 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1012 ms, Memory: 301964 KB -BaekjoonHub

---
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static boolean[] visit, done;
+	static int N, arr[], count;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int TC = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= TC; tc++) {
+			N = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			arr = new int[N + 1];
+			count = 0;
+			for (int i = 1; i <= N; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			visit = new boolean[N + 1];
+			done = new boolean[N + 1];
+			for (int i = 1; i <= N; i++) {
+				if(done[i]) continue;
+				dfs(arr[i]);
+			}
+			System.out.println(N-count);
+		}
+	}
+
+	public static void dfs(int idx) {
+		if(done[idx]) {
+			return;
+		}
+		if(visit[idx]) { // 이미 방문한 노드
+			// 사이클 안에 들어왔다.
+			done[idx] = true;
+			count++;
+		}
+		
+		visit[idx] = true;
+		dfs(arr[idx]);
+		visit[idx] = false;
+        done[idx] = true;
+	}
+}
