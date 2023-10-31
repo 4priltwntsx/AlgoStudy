a7d0aadd90c7dd6c8ddc1b28f8953230c07c3396
김지희 <cjg05034@naver.com>
2023-11-01 00:50:02 +0900

    solving: boj 2217

---
+++ study2023/src/bdfs/BOJ_6593_상범빌딩.java
@@ -0,0 +1,98 @@
+package bdfs;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_6593_상범빌딩 {
+	static class Point {
+		int f;
+		int i, j;
+
+		Point(int f, int i, int j) {
+			this.f = f;
+			this.i = i;
+			this.j = j;
+		}
+	}
+
+	static int L, R, C;
+	static char[][][] map;
+	static int[][][] visit;
+	static Point start, end;
+	static int[] di = { 1, -1, 0, 0, 0, 0 };
+	static int[] dj = { 0, 0, 1, -1, 0, 0 };
+	static int[] df = { 0, 0, 0, 0, 1, -1 };
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		while (true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			L = Integer.parseInt(st.nextToken());
+			R = Integer.parseInt(st.nextToken());
+			C = Integer.parseInt(st.nextToken());
+			if(L==0 && R==0 && C == 0) {
+				System.out.println(sb.toString());
+				break;
+			}
+			map = new char[L][R][C];
+			visit = new int[L][R][C];
+
+			for (int f = 0; f < L; f++) {
+				for (int i = 0; i < R; i++) {
+					map[f][i] = br.readLine().toCharArray();
+					for (int j = 0; j < C; j++) {
+						if (map[f][i][j] == 'S') {
+							start = new Point(f, i, j);
+						}
+						if (map[f][i][j] == 'E') {
+							end = new Point(f, i, j);
+						}
+					}
+				}
+				br.readLine();
+			}
+
+			int res = bfs(start);
+			if (res == Integer.MAX_VALUE)
+				sb.append("Trapped!").append("\n");
+			else
+				sb.append("Escaped in ").append(res-1).append(" minute(s).\n");
+		}
+	}
+
+	public static int bfs(Point start) {
+		Queue<Point> Q = new LinkedList<>();
+		Q.add(start);
+		visit[start.f][start.i][start.j] = 1;
+		int result = Integer.MAX_VALUE;
+		while (!Q.isEmpty()) {
+			Point cur = Q.poll();
+			if (map[cur.f][cur.i][cur.j] == 'E') {
+				result = visit[cur.f][cur.i][cur.j];
+				break;
+			}
+			for (int d = 0; d < 6; d++) {
+				int ni = cur.i + di[d];
+				int nj = cur.j + dj[d];
+				int nf = cur.f + df[d];
+
+				if (ni < 0 || nj < 0 || nf < 0 || ni >= R || nj >= C || nf >= L)
+					continue;
+				if (map[nf][ni][nj] == '#')
+					continue;
+				if (visit[nf][ni][nj] > 0)
+					continue;
+
+				Q.add(new Point(nf, ni, nj));
+				visit[nf][ni][nj] = visit[cur.f][cur.i][cur.j] + 1;
+
+			}
+		}
+		return result;
+	}
+}
---
+++ study2023/src/greedy/BOJ_2217_로프.java
@@ -0,0 +1,32 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ_2217_로프 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		Integer[] arr = new Integer[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		Arrays.sort(arr, new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o2 - o1;
+			}
+		});
+		int answer = 0;
+		for (int i = 0; i < N; i++) {
+			int temp = arr[i] + arr[i] * i;
+			if(answer<temp) {
+				answer = temp;
+			}
+			
+		}
+		System.out.println(answer);
+	}
+}
