22e4452651fe0b08979242253f2b7dca335941c3
Jihee Kim <51085309+4priltwntsx@users.noreply.github.com>
2023-11-02 01:09:36 +0900

    [Gold V] Title: 센서, Time: 216 ms, Memory: 16448 KB -BaekjoonHub

---
+++ 백준/Gold/2212. 센서/센서.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int K = Integer.parseInt(br.readLine());
+
+		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+
+		PriorityQueue<Integer> dist = new PriorityQueue<>(new Comparator<Integer>() {
+			public int compare(Integer o1, Integer o2) {
+				return o2 - o1;
+			}
+		});
+
+		for (int i = 1; i < N; i++) {
+			dist.add(arr[i] - arr[i - 1]);
+		}
+
+		while (K-- > 1) {
+			dist.poll();
+		}
+		int answer = 0;
+		while (!dist.isEmpty()) {
+			answer += dist.poll();
+		}
+		
+		System.out.println(answer);
+	}
+}
