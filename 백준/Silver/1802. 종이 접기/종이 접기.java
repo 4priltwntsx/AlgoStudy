c69a52ad6121497f407f1e0597a669446c6f34d1
Jihee Kim <51085309+4priltwntsx@users.noreply.github.com>
2023-11-07 01:07:25 +0900

    [Silver I] Title: 종이 접기, Time: 132 ms, Memory: 14996 KB -BaekjoonHub

---
+++ 백준/Silver/1802. 종이 접기/종이 접기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int TC = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for (int tc = 1; tc <= TC; tc++) {
+			String now = br.readLine();
+			
+			if(dnc(0, now.length()-1, now)) {
+				sb.append("YES\n");
+			}else {
+				sb.append("NO\n");
+			}
+		}
+		System.out.println(sb.toString());
+	}
+
+	public static boolean dnc(int start, int end, String str) {
+		if (start == end) {
+			return true;
+		}
+		int mid = (start + end) / 2;
+		for (int i = start; i < mid; i++) {
+			if (str.charAt(i) == str.charAt(end - i)) {
+				return false;
+			}
+		}
+		return dnc(start, mid - 1, str) && dnc(mid + 1, end, str);
+	}
+}
