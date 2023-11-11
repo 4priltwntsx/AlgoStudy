fbc66c9a06695089b9b0acc55a3daea44df370e7
Jihee Kim <51085309+4priltwntsx@users.noreply.github.com>
2023-11-11 22:57:12 +0900

    [Silver I] Title: 점프, Time: 132 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Silver/1890. 점프/점프.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		long[][] dp = new long[N][N];
+		int[][] map = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		dp[0][0] = 1;
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				int go = map[i][j];
+				if(go==0) break;
+				if(j+go<N) {
+					dp[i][j+go] += dp[i][j];
+				}
+				if(i+go<N) {
+					dp[i+go][j] += dp[i][j];
+				}
+			}
+		}
+		
+		System.out.println(dp[N-1][N-1]);
+	}
+}
