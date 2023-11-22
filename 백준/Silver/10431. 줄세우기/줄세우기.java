4b2d5e6bf90c3ee2ecc387798e67f0ec9c817f61
Jihee Kim <51085309+4priltwntsx@users.noreply.github.com>
2023-11-22 16:01:27 +0900

    [Silver V] Title: 줄세우기, Time: 220 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int TC = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for(int tc=1; tc<=TC; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int turn = Integer.parseInt(st.nextToken());
+			
+			int[] arr = new int[20];
+			for(int i=0; i<20; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			int answer = check(arr);
+			
+			sb.append(tc).append(" ").append(answer+"\n");
+			
+		}
+		
+		System.out.println(sb.toString());
+	}
+	
+	public static int check(int[] arr) {
+		int cnt=0;
+		for(int i=0 ;i<20; i++) {
+			for(int j=0; j<i; j++) {
+				if(arr[i]<arr[j]) cnt++;
+			}
+		}
+		return cnt;
+	}
+}
